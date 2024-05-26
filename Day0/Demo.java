f840bc7bc820b44b588fbae200d903483a5be805
SHIVAM <shivam@SHIVAMs-MacBook.local>
2024-05-26 17:47:52 +0530

    Day0

---
+++ Day0/Demo.java
@@ -0,0 +1,24 @@
+package Day0;
+
+import java.util.Scanner;
+
+public class Demo {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        // interger input
+        int n = scanner.nextInt();
+        System.out.println(n);
+
+        // character input
+        char ch = scanner.next().charAt(0);
+
+        // String input
+
+        // Integer array input.
+
+        scanner.close();
+    }
+}
